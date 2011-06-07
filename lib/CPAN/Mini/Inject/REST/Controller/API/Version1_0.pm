3bac5f54ba2cc84397c2ecb76ad1c3b7a81b00ea
JJ <jj@opusvl.com>
2011-06-07 11:24:02 +0100

    Prevent duplicate files from being uploaded, index all modules listed in META.json / META.yml

--- lib/CPAN/Mini/Inject/REST/Controller/API/Version1_0.pm
+++ lib/CPAN/Mini/Inject/REST/Controller/API/Version1_0.pm
@@ -1,7 +1,13 @@
 package CPAN::Mini::Inject::REST::Controller::API::Version1_0;
+
+use 5.010;
 use Moose;
+use Archive::Extract;
 use File::Copy;
+use File::Find::Rule;
+use File::Spec::Functions qw/splitpath/;
 use File::Temp;
+use Parse::CPAN::Meta;
 use namespace::autoclean;
 
 BEGIN {extends 'Catalyst::Controller::REST'; }
@@ -43,38 +49,73 @@ sub dist :Local :Args(1) :ActionClass('REST') {}
 sub dist_POST {
     my ($self, $c, $dist) = @_;
     
-    if ($c->req->upload('file')->filename =~ /([^\/]*)-([0-9._]+)\.tar\.gz$/) {
-        my $module = $1;
-        my $version = $2;
-        $module =~ s/-/::/g;
-        
-        my $ftmp    = File::Temp->new();
-        my $tmpdir  = $ftmp->newdir();
-        my $newfile = $tmpdir. "/" . $c->req->upload('file')->filename;
-        copy($c->req->upload('file')->tempname, $newfile);
-        
-        $c->model('CMI')->readlist;
-        $c->model('CMI')->add(
-            module   => $module,
-            version  => $version,
-            authorid => 'LOCAL',
-            file     => $newfile,
-        );
-        $c->model('CMI')->writelist;
-        $c->model('CMI')->inject;
-    }
-    
-    $self->status_created(
-        $c,
-        location => $c->req->uri->as_string,
-        entity   => {
-            filename => $c->req->upload('file')->filename,
-            type     => $c->req->upload('file')->type,
-            tempname => $c->req->upload('file')->tempname,
+    # Check to see if the file already exists
+    $c->model('CMI')->readlist;
+    unless ($c->model('CMI')->{modulelist} && grep {m!L/LO/LOCAL/$dist!} @{$c->model('CMI')->{modulelist}}) {
+        if ($c->req->upload('file')->filename =~ /([^\/]*)-([0-9._]+)\.tar\.gz$/) {
+            my $module = $1;
+            my $version = $2;
+            $module =~ s/-/::/g;
+            
+            my $ftmp    = File::Temp->new();
+            my $tmpdir  = $ftmp->newdir();
+            my $newfile = $tmpdir. "/" . $c->req->upload('file')->filename;
+            copy($c->req->upload('file')->tempname, $newfile);
+            
+            $c->model('CMI')->add(
+                module   => $module,
+                version  => $version,
+                authorid => 'LOCAL',
+                file     => $newfile,
+            );
+            
+            # Add all modules listed in META.json / META.yml
+            if (my $meta = _load_meta($newfile)) {
+                while (my ($module, $details) = each %{$meta->{provides}}) {
+                    $c->model('CMI')->add(
+                        module   => $module,
+                        version  => $details->{version} // 'undef',
+                        authorid => 'LOCAL',
+                        file     => $newfile,
+                    );
+                }
+            }
+            
+            $c->model('CMI')->writelist;
+            $c->model('CMI')->inject;
+            
+            $self->status_created(
+                $c,
+                location => $c->req->uri->as_string,
+                entity   => {
+                    filename => $c->req->upload('file')->filename,
+                    type     => $c->req->upload('file')->type,
+                    tempname => $c->req->upload('file')->tempname,
+                }
+            );
         }
-    )
+    } else {
+        $self->status_bad_request(
+            $c,
+            message => "File $dist already exists",
+        );
+    }
 }
 
+sub _load_meta {
+    my $filename = shift;
+    my ($vol, $dir, $file) = splitpath($filename);
+    my $archive = Archive::Extract->new(archive => $filename);
+    $archive->extract(to => "$vol/$dir");
+    
+    if (my @meta = File::Find::Rule->file->name('META.json')->in("$vol/$dir")) {
+        return Parse::CPAN::Meta->load_file(shift @meta);
+    }
+
+    if (my @meta = File::Find::Rule->file->name('META.yml')->in("$vol/$dir")) {
+        return Parse::CPAN::Meta->load_file(shift @meta);
+    }
+}
 
 =head1 AUTHOR
 
